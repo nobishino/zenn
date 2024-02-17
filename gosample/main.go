4d526e82a456b12390c94185788d98035316dc59
nobishino <nobishii11@gmail.com>
2024-02-17 12:52:50 +0900

    Sample: iter.Pullのユースケース: iter.Seq[int]の内容比較
    https://play.golang.org/p/jrGx5-wkSbd
    
    ```go
    // GOEXPERIMENT=rangefunc
    // (*)実行するとgo vetがエラーを出しますが、実行はできています
    package main
    
    import (
    	"fmt"
    
    	"github.com/nobishino/gocoro/iter"
    )
    
    func main() {
    	s1 := createIntSeq(1, 2, 3, 4, 5)
    	s2 := createIntSeq(1, 2, 3, 5, 4)
    	s3 := createIntSeq(1, 2, 3, 4, 5)
    
    	fmt.Println(equalIntValues(s1, s2)) // false
    	fmt.Println(equalIntValues(s1, s3)) // true
    }
    
    // a, bからえられる値が全て同じかどうかを判定する
    func equalIntValues(a, b iter.Seq[int]) bool {
    	nextA, stopA := iter.Pull(a)
    	defer stopA()
    	nextB, stopB := iter.Pull(b)
    	defer stopB()
    	for {
    		av, aok := nextA()
    		bv, bok := nextB()
    		if av != bv || aok != bok {
    			return false
    		}
    		if !aok {
    			return true
    		}
    	}
    }
    
    func createIntSeq(xs ...int) iter.Seq[int] {
    	return func(yield func(int) bool) {
    		for _, x := range xs {
    			if !yield(x) {
    				break
    			}
    		}
    	}
    }
    ```

--- gosample/main.go
+++ gosample/main.go
@@ -2,21 +2,43 @@
 // (*)実行するとgo vetがエラーを出しますが、実行はできています
 package main
 
-import "github.com/nobishino/gocoro/iter"
+import (
+	"fmt"
+
+	"github.com/nobishino/gocoro/iter"
+)
 
 func main() {
-	next, stop := iter.Pull(seq())
-	defer stop()
-	for v, ok := next(); ok; v, ok = next() {
-		println(v)
+	s1 := createIntSeq(1, 2, 3, 4, 5)
+	s2 := createIntSeq(1, 2, 3, 5, 4)
+	s3 := createIntSeq(1, 2, 3, 4, 5)
+
+	fmt.Println(equalIntValues(s1, s2)) // false
+	fmt.Println(equalIntValues(s1, s3)) // true
+}
+
+// a, bからえられる値が全て同じかどうかを判定する
+func equalIntValues(a, b iter.Seq[int]) bool {
+	nextA, stopA := iter.Pull(a)
+	defer stopA()
+	nextB, stopB := iter.Pull(b)
+	defer stopB()
+	for {
+		av, aok := nextA()
+		bv, bok := nextB()
+		if av != bv || aok != bok {
+			return false
+		}
+		if !aok {
+			return true
+		}
 	}
 }
 
-func seq() iter.Seq[string] {
-	values := []string{"a", "b", "c"}
-	return func(yield func(string) bool) {
-		for i := range 10 {
-			if !yield(values[i%3]) {
+func createIntSeq(xs ...int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for _, x := range xs {
+			if !yield(x) {
 				break
 			}
 		}
