5cf7a92be4132cdd3660fda8e1f1ae6decfb5022
nobishino <nobishii11@gmail.com>
2024-03-27 18:36:12 +0900

    https://play.golang.org/p/HH_HkwsNJWE
    ```go
    package main
    
    import "fmt"
    
    func main() {
    	fmt.Println("Hello, World!")
    }
    ```

--- gommit.sh
+++ gommit.sh
@@ -1,15 +1,15 @@
 
-git diff --name-only | grep "gosample/main.go"
+git show HEAD --name-only | grep "gosample/main.go"
 if [ $? -eq 0 ]; then
   echo "gosample/main.go has been changed. construct the commit message..."
   echo $1 > tmp
   echo "" >> tmp
   pushd gosample
-  echo "$(gp share main.go go.mod go.sum)?v=gotip"  >> ../tmp
+  gp share main.go go.mod go.sum >> ../tmp
   popd 
   echo "" >> tmp
   echo  "\`\`\`go" >> tmp
   cat gosample/main.go >> tmp
   echo  "\`\`\`" >> tmp
-  git commit -am "$(cat tmp)"
+  git commit --amend -am "$(cat tmp)"
 fi
