bb51ec3e0661ddd3cd7752cce113d97e08694bc4
nobishino <nobishii11@gmail.com>
2024-03-27 20:46:16 +0900

    update

--- gosample/main.go
+++ gosample/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type MyStruct struct {
+	X int
+	Y int
+}
+
+var a *MyStruct = &as[0]
+
+var as = [2]MyStruct{
+	{0, 0},
+	{1, 1},
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	go readerGoroutine(a)
+	for i := 0; ; i++ {
+		a = &as[i%2] // 設定更新
+	}
+}
+
+func readerGoroutine(value *MyStruct) {
+	for {
+		v := *value
+		if v.X != v.Y {
+			fmt.Println("Value: ", *value)
+		}
+	}
 }
